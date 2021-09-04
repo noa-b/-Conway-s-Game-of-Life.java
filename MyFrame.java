3bca99f7516da34840838cffa64c8352929f7ac5
noa-b <71805412+noa-b@users.noreply.github.com>
2021-09-04 17:17:45 +0300

    Create MyFrame.java

---
+++ MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.add(new MyPanel());
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
